bc527bd7cd23e11525a197788ec7e98b3969a22b
rohakaryagate6 <130767613+ROHAK8@users.noreply.github.com>
2024-06-14 17:55:02 +0530

    commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,14 @@ import pluginJs from "@eslint/js";
 import { defineConfig } from "eslint-define-config";
 
 export default defineConfig([
-    { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-    { languageOptions: { globals: globals.browser } },
+    {
+        files: ["**/*.js"],
+        languageOptions: { sourceType: "commonjs" },
+        ignores: ["node_modules/", "dist/"],
+    },
+    {
+        languageOptions: { globals: globals.browser },
+        ignores: ["node_modules/", "dist/"],
+    },
     pluginJs.configs.recommended,
 ]);
