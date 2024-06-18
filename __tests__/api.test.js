93978a5364de58cc5330c1d8093cf86e89a44160
rohakaryagate6 <130767613+ROHAK8@users.noreply.github.com>
2024-06-18 21:44:10 +0530

    Update api.test.js

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -14,15 +14,4 @@ describe("API Endpoints", () => {
     expect(res.body).toHaveProperty("message", "Sum is greater than threshold");
     expect(res.body).toHaveProperty("sum", 60);
   });
-
-  it("POST /api/check-sum should return failure if sum is not greater than threshold", async () => {
-    const data = { numbers: [10, 20, 10], threshold: 50 };
-    const res = await request(app).post("/api/check-sum").send(data);
-    expect(res.statusCode).toEqual(400);
-    expect(res.body).toHaveProperty(
-      "message",
-      "Sum is not greater than threshold"
-    );
-    expect(res.body).toHaveProperty("sum", 40);
-  });
 });
